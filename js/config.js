54d4ea73243782deeaf7c54708773bc77b62363d
Michael Poon <michael.l.poon@gmail.com>
2014-08-03 23:05:23 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,23 +1,14 @@
 // for navigator language
-var lang = window.navigator.language;
+// var lang = window.navigator.language;
 // you can change the language
-//var lang = 'en';
+var lang = 'en';
 
 //change weather params here:
 var weatherParams = {
-    'q':'Baarn,Netherlands',
+    'q':'San Francisco,California',
     'units':'metric',
     'lang':lang
 };
 
 // compliments:
-var compliments = [
-            'Hey, handsome!',
-            'Hi, sexy!',
-            'Hello, beauty!',
-            'You look sexy!',
-            'Wow, you look hot!',
-            'Looking good today!',
-            'You look nice!',
-            'Enjoy your day!'
-        ];
+var compliments = [];
